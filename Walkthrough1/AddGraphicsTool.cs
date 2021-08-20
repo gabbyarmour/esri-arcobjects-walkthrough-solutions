43716937eb03e67cea8fb7b6f9ff62ea8eb43ef7
galacticgerbil <49649533+gabbyarmour@users.noreply.github.com>
2021-08-20 16:45:08 -0500

    Add files via upload

---
+++ Walkthrough1/AddGraphicsTool.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geometry;
+
+namespace AddInCustomUIElements
+{
+    public class AddGraphicsTool : ESRI.ArcGIS.Desktop.AddIns.Tool
+    {
+        public AddGraphicsTool()
+        {
+        }
+
+        protected override void OnMouseDown(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
+        {
+            //Get the active view from the ArcMap static class.
+            IActiveView activeView = ArcMap.Document.ActiveView;
+            //If it's a polyline object, get from the user's mouse clicks.
+            IPolyline polyline = GetPolylineFromMouseClicks(activeView);
+            //Make a color to draw the polyline. 
+
+            IRgbColor rgbColor = new RgbColorClass();
+            rgbColor.Red = 255;
+            //Add the user's drawn graphics as persistent on the map.
+            AddGraphicToMap(activeView.FocusMap, polyline, rgbColor, rgbColor);
+            //Best practice: Redraw only the portion of the active view that contains graphics. 
+            activeView.PartialRefresh(esriViewDrawPhase.esriViewGraphics, null, null);
+        }
+        
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+
+        #region
+        public void AddGraphicToMap(ESRI.ArcGIS.Carto.IMap map, ESRI.ArcGIS.Geometry.IGeometry geometry, ESRI.ArcGIS.Display.IRgbColor rgbColor, ESRI.ArcGIS.Display.IRgbColor outlineRgbColor)
+        {
+            ESRI.ArcGIS.Carto.IGraphicsContainer graphicsContainer = (ESRI.ArcGIS.Carto.IGraphicsContainer)map; // Explicit Cast
+            ESRI.ArcGIS.Carto.IElement element = null;
+            if ((geometry.GeometryType) == ESRI.ArcGIS.Geometry.esriGeometryType.esriGeometryPoint)
+            {
+                // Marker symbols
+                ESRI.ArcGIS.Display.ISimpleMarkerSymbol simpleMarkerSymbol = new ESRI.ArcGIS.Display.SimpleMarkerSymbolClass();
+                simpleMarkerSymbol.Color = rgbColor;
+                simpleMarkerSymbol.Outline = true;
+                simpleMarkerSymbol.OutlineColor = outlineRgbColor;
+                simpleMarkerSymbol.Size = 15;
+                simpleMarkerSymbol.Style = ESRI.ArcGIS.Display.esriSimpleMarkerStyle.esriSMSCircle;
+
+                ESRI.ArcGIS.Carto.IMarkerElement markerElement = new ESRI.ArcGIS.Carto.MarkerElementClass();
+                markerElement.Symbol = simpleMarkerSymbol;
+                element = (ESRI.ArcGIS.Carto.IElement)markerElement; // Explicit Cast
+            }
+            else if ((geometry.GeometryType) == ESRI.ArcGIS.Geometry.esriGeometryType.esriGeometryPolyline)
+            {
+                //  Line elements
+                ESRI.ArcGIS.Display.ISimpleLineSymbol simpleLineSymbol = new ESRI.ArcGIS.Display.SimpleLineSymbolClass();
+                simpleLineSymbol.Color = rgbColor;
+                simpleLineSymbol.Style = ESRI.ArcGIS.Display.esriSimpleLineStyle.esriSLSSolid;
+                simpleLineSymbol.Width = 5;
+
+                ESRI.ArcGIS.Carto.ILineElement lineElement = new ESRI.ArcGIS.Carto.LineElementClass();
+                lineElement.Symbol = simpleLineSymbol;
+                element = (ESRI.ArcGIS.Carto.IElement)lineElement; // Explicit Cast
+            }
+            else if ((geometry.GeometryType) == ESRI.ArcGIS.Geometry.esriGeometryType.esriGeometryPolygon)
+            {
+                // Polygon elements
+                ESRI.ArcGIS.Display.ISimpleFillSymbol simpleFillSymbol = new ESRI.ArcGIS.Display.SimpleFillSymbolClass();
+                simpleFillSymbol.Color = rgbColor;
+                simpleFillSymbol.Style = ESRI.ArcGIS.Display.esriSimpleFillStyle.esriSFSForwardDiagonal;
+                ESRI.ArcGIS.Carto.IFillShapeElement fillShapeElement = new ESRI.ArcGIS.Carto.PolygonElementClass();
+                fillShapeElement.Symbol = simpleFillSymbol;
+                element = (ESRI.ArcGIS.Carto.IElement)fillShapeElement; // Explicit Cast
+            }
+            if (!(element == null))
+            {
+                element.Geometry = geometry;
+                graphicsContainer.AddElement(element, 0);
+            }
+        }
+        #endregion
+
+        #region
+        public IPolyline GetPolylineFromMouseClicks(ESRI.ArcGIS.Carto.IActiveView activeView)
+        {
+
+                IScreenDisplay screenDisplay = activeView.ScreenDisplay;
+
+                IRubberBand rubberBand = new RubberLineClass();
+                IGeometry geometry = rubberBand.TrackNew(screenDisplay, null);
+
+                IPolyline polyline = (IPolyline) geometry;
+
+                return polyline;
+
+        }
+        #endregion
+    }
+}
---
+++ Walkthrough1/ZoomToLayerButton.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geometry;
+
+namespace AddInCustomUIElements
+{
+    public class ZoomToLayerButton : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public ZoomToLayerButton()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            ZoomToActiveLayerInTOC(ArcMap.Application.Document as IMxDocument);
+            ArcMap.Application.CurrentTool = null;
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+
+        #region
+        public void ZoomToActiveLayerInTOC(ESRI.ArcGIS.ArcMapUI.IMxDocument mxDocument)
+        {
+            if (mxDocument == null)
+            {
+                return;
+            }
+            ESRI.ArcGIS.Carto.IActiveView activeView = mxDocument.ActiveView;
+
+            // Get the TOC
+            ESRI.ArcGIS.ArcMapUI.IContentsView IContentsView = mxDocument.CurrentContentsView;
+
+            // Get the selected layer
+            System.Object selectedItem = IContentsView.SelectedItem;
+            if (!(selectedItem is ESRI.ArcGIS.Carto.ILayer))
+            {
+                return;
+            }
+            ESRI.ArcGIS.Carto.ILayer layer = selectedItem as ESRI.ArcGIS.Carto.ILayer;
+
+
+            // Zoom to the extent of the layer and refresh the map
+            activeView.Extent = layer.AreaOfInterest;
+            activeView.Refresh();
+        }
+        #endregion
+    }
+
+}
